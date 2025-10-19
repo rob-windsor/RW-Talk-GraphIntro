a2c7761bd4116c547c18b2574d850e19229dd64a
Rob Windsor <rob.windsor@gmail.com>
2025-10-19 08:45:18 -0400

    Add SPFx demos

---
+++ Demos/SpfxGraphPnp/src/webparts/spfxGraphPnp/SpfxGraphPnpWebPart.ts
@@ -0,0 +1,135 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  type IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import type { IReadonlyTheme } from '@microsoft/sp-component-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import { GraphFI, graphfi, SPFx } from "@pnp/graph";
+import * as MicrosoftGraph from '@microsoft/microsoft-graph-types';
+import "@pnp/graph/groups";
+
+import styles from './SpfxGraphPnpWebPart.module.scss';
+import * as strings from 'SpfxGraphPnpWebPartStrings';
+
+export interface ISpfxGraphPnpWebPartProps {
+  description: string;
+}
+
+export default class SpfxGraphPnpWebPart extends BaseClientSideWebPart<ISpfxGraphPnpWebPartProps> {
+
+  private _isDarkTheme: boolean = false;
+  private _environmentMessage: string = '';
+  private _graph: GraphFI;
+
+  public render(): void {
+    this.domElement.innerHTML = `
+    <section class="${styles.spfxGraphPnp} ${!!this.context.sdks.microsoftTeams ? styles.teams : ''}">
+      <div class="${styles.welcome}">
+        <img alt="" src="${this._isDarkTheme ? require('./assets/welcome-dark.png') : require('./assets/welcome-light.png')}" class="${styles.welcomeImage}" />
+        <h2>Well done, ${escape(this.context.pageContext.user.displayName)}!</h2>
+        <div>${this._environmentMessage}</div>
+        <div>Web part property value: <strong>${escape(this.properties.description)}</strong></div>
+      </div>
+      <div>
+        <p class="ms-font-l">
+          <div id="groupsContainer"></div>
+        </p>           
+      </div>
+    </section>`;
+
+    this._graph.groups().then((value: MicrosoftGraph.Group[]) => {
+      let elem: Element | null = this.domElement.querySelector("#groupsContainer");
+      if (elem) {
+        elem.innerHTML = value.map((group) => {
+          return `<span>${group.displayName}</span>`;
+        }).join(`<br />`);
+      }
+    });
+  }
+
+  protected onInit(): Promise<void> {
+    this._graph = graphfi().using(SPFx(this.context));
+    this._getEnvironmentMessage()
+      .then((message) => {
+        this._environmentMessage = message;
+      });
+
+    return super.onInit();
+  }
+
+
+
+  private _getEnvironmentMessage(): Promise<string> {
+    if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
+      return this.context.sdks.microsoftTeams.teamsJs.app.getContext()
+        .then(context => {
+          let environmentMessage: string = '';
+          switch (context.app.host.name) {
+            case 'Office': // running in Office
+              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOffice : strings.AppOfficeEnvironment;
+              break;
+            case 'Outlook': // running in Outlook
+              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOutlook : strings.AppOutlookEnvironment;
+              break;
+            case 'Teams': // running in Teams
+            case 'TeamsModern':
+              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+              break;
+            default:
+              environmentMessage = strings.UnknownEnvironment;
+          }
+
+          return environmentMessage;
+        });
+    }
+
+    return Promise.resolve(this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment);
+  }
+
+  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
+    if (!currentTheme) {
+      return;
+    }
+
+    this._isDarkTheme = !!currentTheme.isInverted;
+    const {
+      semanticColors
+    } = currentTheme;
+
+    if (semanticColors) {
+      this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null);
+      this.domElement.style.setProperty('--link', semanticColors.link || null);
+      this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
+    }
+
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ Demos/SpfxGraphPnp/src/webparts/spfxGraphPnp/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISpfxGraphPnpWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SpfxGraphPnpWebPartStrings' {
+  const strings: ISpfxGraphPnpWebPartStrings;
+  export = strings;
+}
